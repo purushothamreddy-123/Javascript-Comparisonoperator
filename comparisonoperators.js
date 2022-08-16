1e2f3b73636d9797fbad391bd3afd96ef39b5720
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-16 18:00:17 +0530

    Javascript comparisonoperators

---
+++ comparisonoperators.js
@@ -0,0 +1,43 @@
+var x = 5, y = 2, z = 'hello';
+
+// equal to operator
+console.log(x == 5);    
+console.log(y == '2');   
+console.log(z == 'Hello');
+
+
+// var a = 3, b = 'hello';
+
+// // not equal operator
+// console.log(a != 2); 
+// console.log(b != 'Hello');
+
+// var a = 2;
+
+// // strict equal operator
+// console.log(a === 2); 
+// console.log(a === '2');
+
+// var a = 3;
+
+// // greater than operator
+// console.log(a > 2);
+
+
+// var a = 3;
+
+// // greater than or equal operator
+// console.log(a >= 3);
+
+
+// var x= 3, y = 2;
+
+// // less than operator
+// console.log(x < 2); 
+// console.log(y < 3);
+
+// var a = 2;
+
+// // less than or equal operator
+// console.log(a <= 3) 
+// console.log(a <= 2);
